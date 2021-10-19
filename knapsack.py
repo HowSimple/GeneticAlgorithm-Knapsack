c7a70f3f3e2ac61b98fae3745af469fdb3b9856a
Caleb Vaughn <43933950+HowSimple@users.noreply.github.com>
2021-10-19 10:24:16 -0500

    Update knapsack.py

--- knapsack.py
+++ knapsack.py
@@ -1,25 +1,28 @@
-import random
+
+import numpy as np
+
 
 class GeneticAlgorithm:
 
-    def __init__(self, inputFile, items, weight, mutationRate, initialPopulation):
+    def __init__(self, inputFile, items, weight, mutationRate, initialPopulation, generations):
+        self.generations = generations
         self.maxWeight = weight
         self.numberOfItems = items
         self.mutationRate = mutationRate
-        self.initialPopulation = initialPopulation
+        self.populationSize = initialPopulation
         self.items = []
         self.chromosome = []
-
+        self.population =[]
         self.read(inputFile)
+        self.initialPopulation()
+        self.fitness()
 
     def initialPopulation(self):
+        selections_per_individual = 3
+        self.population = [np.random.randint(0, 2,  self.numberOfItems).tolist() for _ in range(self.populationSize)]
+        #self.population = np.random.randint(2, size = self.populationSize)
 
-        population = []
-        for x in range(self.initialPopulation):
-
-            population.add()
-
-
+        print('Initial population: \n{}'.format( self.population ))
 
 
     def read(self, filename):
@@ -29,28 +32,30 @@ class GeneticAlgorithm:
 
         return data
 
-
     def selection(self):
-        
+        self.chronosome
+
     def fitness(self):
         # total utility of 400 selections
-        for item in self.items:
-            if self.chromosome[item] == True:
-                total += item.utility
+        fitness = np.zeros(self.numberOfItems)
 
-        return total
+        # for indv in range(self.populationSize):
+        for i in range(self.populationSize):
+            fitness[i] = sum(np.multiply(self.items[0], self.population[i]))
+            if (fitness[i]  > self.maxWeight):
+                fitness[i] = 0
+
+
+        total_fitness = sum(fitness)
+        return total_fitness
 
 
 
-        total = sum()
 
-    def fitness(self, value, weight):
-        if (weight > max):
-            return 1
-        else:
-            return 0
+
     def mutation(self, population):
         return
+
     def crossover(self, parent1, parent2, crossoverProbability):
         child1 = parent1.copy
         child2 = parent2.copy
@@ -58,23 +63,14 @@ class GeneticAlgorithm:
         if random() < crossoverProbability:
             print()
 
+        # random crossover point
 
-
-        #random crossover point
-
-
-    def selection(self, fitness ):
+    def selection(self, fitness):
 
         for x in range(self.populationSize):
             print()
 
-
-
 if __name__ == '__main__':
     print('Starting...')
-    print( GeneticAlgorithm("Program2Input.txt", 400, 500, 0.0001, 1000).read("Program2Input.txt"))
 
-
-
-
-
+    print(GeneticAlgorithm("Program2Input.txt", 400, 500, 0.0001, 1000, 5000).read("Program2Input.txt"))
